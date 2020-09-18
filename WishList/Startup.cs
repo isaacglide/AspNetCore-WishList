af8f6cb9f510c22e0bdfcc85fe1c3863ef63c1bc
Isaac Glidewell <isaac.j.glidewell@emcins.com>
2020-09-18 09:41:11 -0500

    Remove app.Run from Configure method

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -22,10 +22,6 @@ namespace WishList
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
         }
     }
 }
