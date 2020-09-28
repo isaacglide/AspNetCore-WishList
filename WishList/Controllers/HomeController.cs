25fa43831d0336f4339e632506478d93d0428097
Isaac Glidewell <isaac.j.glidewell@emcins.com>
2020-09-28 18:36:51 -0500

    update again

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -4,18 +4,3 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WishList.Controllers
-{
-    public class HomeController : Controllers
-    {
-        public IActionResult Index()
-        {
-            return View("Index");
-        }
-
-        public IActionResult Error()
-        {
-            return View("Error");
-        }
-    }
-}
