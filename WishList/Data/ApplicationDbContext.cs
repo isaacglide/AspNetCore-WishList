a7976ee647183e0482140e7ecb0fb16e2fd05d6c
Isaac Glidewell <isaac.j.glidewell@emcins.com>
2020-09-28 18:59:46 -0500

    Step 4 complete

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -1,3 +1,15 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
 
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Models.Item> Items { get; set; }
+    }
+}
 
